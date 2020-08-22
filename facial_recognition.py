9ffeb38ba25c143db293c548cbb990a9781928a8
michael-yang2 <michael-yang2@github.com>
2020-08-22 09:15:54 -0700

    Various helper functions

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,1 +1,17 @@
 import face_recognition
+from PIL import image
+
+
+def load_image(path_to_img):
+	image = face_recognition.load_image_file()
+	return image
+def get_face_locations(image):
+	face_locations = face_recognition.face_locations(image)
+	return face_locations
+
+def compare_faces(list_of_training_imgs, img2):
+	face1 = []
+	for traning_img in list_of_training_imgs:
+		face1.append(face_recognition.face_encodings(training_img)[0])
+	face2 = face_recognition.face_encodings(img2)[0]
+	return face_recognition.compare_faces(face,face2, tolerance = 0.6)
