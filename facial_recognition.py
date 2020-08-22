bc7601ea15912b842d5930c17302c6e31567167e
michael-yang2 <michael-yang2@github.com>
2020-08-22 09:36:37 -0700

    face labels

--- facial_recognition.py
+++ facial_recognition.py
@@ -1,5 +1,5 @@
 import face_recognition
-from PIL import image
+from PIL import Image, ImageDraw
 
 
 def load_image(path_to_img):
@@ -8,10 +8,33 @@ def load_image(path_to_img):
 def get_face_locations(image):
 	face_locations = face_recognition.face_locations(image)
 	return face_locations
-
 def compare_faces(list_of_training_imgs, img2):
 	face1 = []
 	for traning_img in list_of_training_imgs:
 		face1.append(face_recognition.face_encodings(training_img)[0])
 	face2 = face_recognition.face_encodings(img2)[0]
 	return face_recognition.compare_faces(face,face2, tolerance = 0.6)
+def pull_faces(img):
+	face_locations = get_face_locations(image)
+	for face in face_locations:
+		top, right, bottom left = face
+		cropped = img[top:bottom, left:right]
+		pil_img = Image.fromarray(cropped)
+		pil_img.save(f'{top}.jpg')
+def label_faces(img, known_encodings, known_names):
+	face_locations = get_face_locations(img)
+	face_encodings = face_recognition.face_encodings(img,face_locations)
+	pil_img = Image.fromarray(img)
+	draw = ImageDraw.Draw(pil_img)
+	for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+		matches = face_recognition.compare_faces(known_encodings, face_encoding)
+		name = "Unknown Person"
+		if True in matches:
+			match_index = matches.index(True)
+			name = known_names[match_index]
+		draw.rectangle(((left,top),(right,bottom)),outline = (0,0,0))
+		text_width, text_height = draw.textsize(name)
+		draw.rectangle(((left, bottom-textheight-10), (right, bottom)), fill=(0,0,0), outline = (0,0,0))
+		draw.text((left+10, bottom - textheight - 5), name, fill = (255,255,255,255))
+	del draw
+	 
