1da9477681ad634ccea976131c95c003f95eb9f1
michael-yang2 <michaelyang2k8@gmail.com>
2020-08-27 11:07:11 -0700

    labelling now works as expected

--- facial_recognition.py
+++ facial_recognition.py
@@ -9,26 +9,23 @@ from PIL import Image, ImageDraw
 def load_image(path_to_img):
 	image = face_recognition.load_image_file(path_to_img)
 	return image
-def get_face_locations(img_file):
-	image = load_image(img_file)
-	face_locations = face_recognition.face_locations(image)
+def get_face_locations(img):
+	face_locations = face_recognition.face_locations(img)
 	return face_locations
 def compare_faces(list_of_training_imgs, img2):
 	face1 = []
 	for training_img in list_of_training_imgs:
-		face1.append(face_recognition.face_encodings(load_image(training_img))[0])
-	face2 = face_recognition.face_encodings(load_image(img2))[0]
+		face1.append(face_recognition.face_encodings(training_img)[0])
+	face2 = face_recognition.face_encodings(img2)[0]
 	return face_recognition.compare_faces(face,face2, tolerance = 0.6)
-def pull_faces(img_file):
-	img = load_image(img_file)
+def pull_faces(img):
 	face_locations = get_face_locations(image)
 	for face in face_locations:
 		top, right, bottom, left = face
 		cropped = img[top:bottom, left:right]
 		pil_img = Image.fromarray(cropped)
 		pil_img.save(f'{top}.jpg')
-def label_faces(img_file, known_encodings, known_names):
-	img = load_image(img_file)
+def label_faces(img, known_encodings, known_names,file_to_write):
 	face_locations = get_face_locations(img)
 	face_encodings = face_recognition.face_encodings(img,face_locations)
 	pil_img = Image.fromarray(img)
@@ -41,9 +38,9 @@ def label_faces(img_file, known_encodings, known_names):
 			name = known_names[match_index]
 		draw.rectangle(((left,top),(right,bottom)),outline = (0,0,0))
 		text_width, text_height = draw.textsize(name)
-		draw.rectangle(((left, bottom-textheight-10), (right, bottom)), fill=(0,0,0), outline = (0,0,0))
-		draw.text((left+10, bottom - textheight - 5), name, fill = (255,255,255,255))
-	pil_img.show()
+		draw.rectangle(((left, bottom-text_height-10), (right, bottom)), fill=(0,0,0), outline = (0,0,0))
+		draw.text((left+10, bottom - text_height - 5), name, fill = (255,255,255,255))
+	pil_img.save(file_to_write)
 	del draw
 def add_labels(list_of_img_files, list_of_labels, file_to_write = "./labels.json", overwrite = True):
 	"""
@@ -83,6 +80,11 @@ def pull_labels(file = "./labels.json"):
 		print("File not found")
 		return [[],[]]
 	return [data["labels"],np.array(data["encodings"])]
+def run():
+	labels, encodings = pull_labels()
+	for img_file in os.listdir('./imgs'):
+		img = load_image('./imgs/'+img_file)
+		label_faces(img, encodings, labels, './dump/labeled_'+img_file)
 
 
 
